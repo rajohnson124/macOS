1d606f3371ed2a6e8c6b4362c68d820c05d8e3dc
rajohnson124 <81185765+rajohnson124@users.noreply.github.com>
2024-09-17 16:31:51 -0400

    Update install_chrome.sh

--- install_chrome.sh
+++ install_chrome.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
-# Script to download and install Google Chrome.
+# Script to check for, download, and install Google Chrome.
 
 dmgfile="googlechrome.dmg"
 volname="Google Chrome"
 logfile="/Library/Logs/GoogleChromeInstallScript.log"
-
+chrome_path="/Applications/Google Chrome.app"
 Url='https://dl.google.com/dl/chrome/mac/universal/stable/gcea/googlechrome.dmg'
+
+# Check if Google Chrome is already installed
+if [ -d "${chrome_path}" ]; then
+    /bin/echo "$(date): Google Chrome is already installed." >> ${logfile}
+    exit 0
+fi
 
 # Download and install
 /bin/echo "$(date): Downloading latest version." >> ${logfile}
@@ -13,7 +19,7 @@ Url='https://dl.google.com/dl/chrome/mac/universal/stable/gcea/googlechrome.dmg'
 /bin/echo "$(date): Mounting installer disk image." >> ${logfile}
 /usr/bin/hdiutil attach /tmp/${dmgfile} -nobrowse -quiet
 /bin/echo "$(date): Installing..." >> ${logfile}
-ditto -rsrc "/Volumes/${volname}/Google Chrome.app" "/Applications/Google Chrome.app"
+ditto -rsrc "/Volumes/${volname}/Google Chrome.app" "${chrome_path}"
 /bin/sleep 10
 /bin/echo "$(date): Unmounting installer disk image." >> ${logfile}
 /usr/bin/hdiutil detach $(/bin/df | /usr/bin/grep "${volname}" | awk '{print $1}') -quiet
